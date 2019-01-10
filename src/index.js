451b6479e027e3d3b549104bbf8a653891e0af74
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-09 20:23:58 -0500

    basic component structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+/* eslint-disable no-console */
 
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const Fancy = (props) => {
-    const { type } = props
-    return (
-        <div>{ type }</div>
-    )
+class Fancy extends Component {
+    componentDidMount() {
+        console.log('test')
+    }
+
+    render() {
+        const { type, value } = this.props
+        return (
+            <div>
+                <h1>{type}</h1>
+                <p>{value}</p>
+            </div>
+        )
+    }
 }
 export default Fancy
 
 Fancy.propTypes = {
     type: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
 }
